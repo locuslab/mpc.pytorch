63732fa85ab2a151045493c4e67653210ca3d7ff
Brandon Amos <brandon.amos.cs@gmail.com>
2022-11-10 19:39:47 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
     url='https://github.com/locuslab/mpc.pytorch',
     packages=find_packages(),
     install_requires=[
-        'numpy>=1<2', 'torch'
+        'numpy>=1,<2', 'torch'
     ]
 )
